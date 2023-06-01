3ab2740d2df0884ba6d76bd13e7e421cf8f26f2e
Gracerue <munyaradzi045@gmail.com>
2023-06-01 15:35:35 +0200

    Create solver's class

--- solver.rb
+++ solver.rb
@@ -1,2 +1,24 @@
-require_relative 'solver.rb'
+class Solver 
+  def factorial(n)
+    raise ArgumentError, "Number must not be negative" if n < 0
+    return 1 if n == 0 || n == 1
+    n * factorial(n - 1)
+  end
 
+  def reverse_string(word)
+    reverse = word.reverse
+    reverse
+  end
+
+  def fizz_buzz(n)
+    if n % 3 == 0 && n % 5 != 0
+      return 'Fizz'
+    elsif n % 5 == 0 && n % 3 != 0
+      return 'Buzz'
+    elsif n % 3 == 0 && n % 5 == 0
+      return 'FizzBuzz'
+    else
+      return n.to_s
+    end
+  end
+end
